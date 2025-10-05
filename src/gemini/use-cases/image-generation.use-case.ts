34ca02728334b85cb8cb3faf51bcdf495617615a
Miguel Angel Molina Garzon <miguel11881@gmail.com>
2025-10-04 22:54:31 -0500

    feat:  image generation use case with multi-modal response handling

--- src/gemini/use-cases/image-generation.use-case.ts
+++ src/gemini/use-cases/image-generation.use-case.ts
@@ -1,7 +1,8 @@
-import { Content, createPartFromUri, GoogleGenAI } from "@google/genai";
+import { Content, ContentListUnion, createPartFromUri, GoogleGenAI, Modality } from "@google/genai";
+
 import {geminiUploadFiles } from "../helpers/gemini-upload-file";
 import { ImageGenerationDto } from "../dtos/image-generation.dto";
-import { text } from "stream/consumers";
+
 
 
 interface Options{
@@ -19,16 +20,47 @@ export const imageGenerationUseCase = async (
     options?: Options,
     ): Promise<ImageGenerationResponse> => {
         const {prompt, files = [] } = imageGenerationDto;
+        const contents: ContentListUnion = [{text: prompt}];
+
         const images = await geminiUploadFiles(ai, files);
 
-        const {
-            model = "gemini-2.5-flash",
-            systemInstruction = `
-            Responde únicamente en español, en formato Markdown
-            Usa negritas de esta forma __
-            Y usando el sistema métrico decimal`,
-    } = options ?? {};
+        images.forEach(file => {
+            contents.push(
+                createPartFromUri(file.uri ?? '', file.mimeType?? ''));
+        });
+        const {model = "gemini-2.0-flash-exp-image-generation"} = options ?? {};
 
+        const response = await ai.models.generateContent({
+            model: model,
+            contents: contents,
+            config:{
+                responseModalities: [Modality.TEXT, Modality.IMAGE]
+            }
+        });
+
+        console.log(response);
+
+        let imageUrl = '';
+        let text= '';
+
+        const { v4: uuidV4 } = await import("uuid");
+        const imageId = uuidV4();
+
+    for (const part of response .candidates?.[0]?.content?.parts ?? []){
+        if(part.text){
+            text = part.text;
+            continue;
+        }
+        if (!part.inlineData){
+            continue;
+        }
+
+        const imageData = part.inlineData.data!;
+        const  buffer = Buffer.from(imageData, 'base64');
+        console.log(buffer);
+    }
+
+    console.log({text});
     return {
         imageUrl: 'xxxax',
         text: 'ABC'
